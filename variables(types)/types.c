4315d7e46efc0e5f90e68d52b8d0134a05bf2091
JeanVincentz <jean.vincentz@outlook.com>
2020-09-20 15:03:16 +0200

    Mémoires, Variables, Variables(types)
    Co-Authored-By: Jean Vincentz <55762464+JeanVincentz@users.noreply.github.com>

---
+++ variables(types)/types.c
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+
+int main(int argc, char *argv[]){ // Équivalent de int main()
+    
+    int noteDeMaths = 10;
+    noteDeMaths = 10;
+
+    double sommeArgentRecue = 60; 
+    sommeArgentRecue = 60;
+
+    unsigned int nombreDeLecteursEnTrainDeLireUnNomDeVariableUnPeuLong = 19;
+    nombreDeLecteursEnTrainDeLireUnNomDeVariableUnPeuLong = 19;
+
+    int nombreDeVies = 5;
+    nombreDeVies = 5;
+
+    return 0; 
+}
